f2e11f515dbbc840369c3d4a946d61366dc6dd9d
Da Xin <da.xin@wsu.edu>
2020-04-12 20:07:37 -0700

    issue 33428 Data links in Pandas API Reference are broken 404

--- pandas/plotting/_misc.py
+++ pandas/plotting/_misc.py
@@ -358,8 +358,7 @@ def parallel_coordinates(
     Examples
     --------
     >>> from matplotlib import pyplot as plt
-    >>> df = pd.read_csv('https://raw.github.com/pandas-dev/pandas/master'
-                        '/pandas/tests/data/csv/iris.csv')
+    >>> df = pd.read_csv('https://raw.github.com/pandas-dev/pandas/master/pandas/tests/data/iris.csv')
     >>> pd.plotting.parallel_coordinates(
             df, 'Name',
             color=('#556270', '#4ECDC4', '#C7F464'))
